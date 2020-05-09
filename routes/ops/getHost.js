b730bcddc36c2523b12cd2b478a1a250ef6666fb
Epaphradito Lugayavu <paphra.me@gmail.com>
2020-05-10 00:40:45 +0300

    Made Some changes

--- routes/ops/getHost.js
+++ routes/ops/getHost.js
@@ -2,7 +2,7 @@
 module.exports = function (req){
 	let host = req.protocol + '://' + req.hostname;
   let port = ':3000'; // for dev
-  if(req.hostname === 'localhost' || '127.0.0.1'){
+  if(req.hostname === 'localhost' || req.hostname === '127.0.0.1'){
     host += port;
 	}
 	
